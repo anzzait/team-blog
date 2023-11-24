c97c4ac3c46c8998675e9785326c900dbe4eb55f
riotamoriya <riotamoriya@gmail.com>
2023-11-24 14:19:16 +0900

    Updates

--- component---src-pages-404-js-89e9456219d654e5c4ce.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcontentful_starter_gatsby_blog=self.webpackChunkcontentful_starter_gatsby_blog||[]).push([[883],{429:function(e,t,n){n.r(t);var l=n(7294),s=n(3544);t.default=function(){return l.createElement(s.Z,null,l.createElement("div",{style:{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}},l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-89e9456219d654e5c4ce.js.map
